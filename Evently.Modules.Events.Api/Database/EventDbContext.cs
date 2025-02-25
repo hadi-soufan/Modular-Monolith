1818f8ce7c7fe0fa3815dd86b1f28a711e155bed
hadi-soufan <hady.a.soufan@gmail.com>
2025-02-25 18:45:18 +0200

    DB: added db context

--- Evently.Modules.Events.Api/Database/EventDbContext.cs
+++ Evently.Modules.Events.Api/Database/EventDbContext.cs
@@ -2,9 +2,11 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace Evently.Modules.Events.Api.Database;
+
 public sealed class EventDbContext(DbContextOptions<EventDbContext> options) : DbContext(options)
 {
-    internal DbSet<Event> Events { get; set; } = null!;
+    internal DbSet<Event> Events { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Events);
