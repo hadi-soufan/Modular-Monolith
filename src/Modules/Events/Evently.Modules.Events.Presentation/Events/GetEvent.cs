8c08b5c38f6a109633811bbcc000b1c03a37fbcc
hadi-soufan <hady.a.soufan@gmail.com>
2025-11-15 10:01:30 +0200

    EVENTS: added more use cases, cross cutting concerns classes, entities...

--- src/Modules/Events/Evently.Modules.Events.Presentation/Events/GetEvent.cs
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Events/GetEvent.cs
@@ -1,22 +1,22 @@
-﻿using Evently.Modules.Events.Application.Events;
-using Evently.Modules.Events.Application.Events.GetEvent;
-using Evently.Modules.Presentation.Api;
+﻿using Evently.Modules.Events.Application.Events.GetEvent;
+using Evently.Modules.Events.Domain.Abstractions;
+using Evently.Modules.Events.Presentation.ApiResults;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 namespace Evently.Modules.Events.Presentation.Events;
+
 internal static class GetEvent
 {
     public static void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapGet("events/{id}", async (Guid id, ISender sender) =>
         {
+            Result<EventResponse> result = await sender.Send(new GetEventQuery(id));
 
-            EventResponse @event = await sender.Send(new GetEventQuery(id));
-
-            return @event is null ? Results.NotFound() : Results.Ok(@event);
+            return result.Match(Results.Ok, ApiResults.ApiResults.Problem);
         })
         .WithTags(Tags.Events);
     }
--- src/Modules/Events/Evently.Modules.Events.Presentation/Tags.cs
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Tags.cs
@@ -1,5 +1,8 @@
-﻿namespace Evently.Modules.Presentation.Api;
+﻿namespace Evently.Modules.Events.Presentation;
+
 internal static class Tags
 {
     internal const string Events = "Events";
+    internal const string TicketTypes = "TicketTypes";
+    internal const string Categories = "Categories";
 }
