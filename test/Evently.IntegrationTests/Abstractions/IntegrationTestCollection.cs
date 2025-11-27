e63b7628dea6bbbd18f3a6ffc62881cb3b3d4dc3
hadi-soufan <hady.a.soufan@gmail.com>
2025-11-27 15:51:59 +0200

    TEST: added system integration tests

---
+++ test/Evently.IntegrationTests/Abstractions/IntegrationTestCollection.cs
@@ -0,0 +1,6 @@
+﻿namespace Evently.IntegrationTests.Abstractions;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1515:Consider making public types internal", Justification = "Base class needs to be public for test inheritance across test assemblies")]
+
+[CollectionDefinition(nameof(IntegrationTestCollection))]
+public sealed class IntegrationTestCollection : ICollectionFixture<IntegrationTestWebAppFactory>;
