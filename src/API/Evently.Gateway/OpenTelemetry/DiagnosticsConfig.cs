f3a5999d72f636395584caadfcb3b13d477072be
hadi-soufan <hady.a.soufan@gmail.com>
2025-11-29 10:57:01 +0200

    INFRA: added api gateway with yarp

---
+++ src/API/Evently.Gateway/OpenTelemetry/DiagnosticsConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Evently.Gateway.OpenTelemetry;
+
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1515:Consider making public types internal", Justification = "Base class needs to be public for test inheritance across test assemblies")]
+
+public static class DiagnosticsConfig
+{
+    public const string ServiceName = "Evently.Gateway";
+}
